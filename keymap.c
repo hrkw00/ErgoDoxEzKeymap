83eed1b9bf6304ce215dcf19544585f75ac42524
hirakawa <hiraryo.1019@gmail.com>
2021-01-27 00:46:33 +0900

    update

--- keymap.c
+++ keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),
   KC_LCTL,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,
   KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
-  LT(SYMB,KC_GRV), KC_QUOT,     KC_LALT,       KC_LCMD, TT(SYMB),
+  LT(SYMB,KC_GRV), KC_QUOT,     KC_LALT,       KC_LCMD, MO(SYMB),
                                                            ALT_T(KC_APP), KC_LGUI,
                                                                           KC_HOME,
                                                           KC_ENT, KC_BSPC, KC_END,
